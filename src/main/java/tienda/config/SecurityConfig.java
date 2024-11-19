be2c35f22a80c335882e513d52b12df817954c1f
Antonio <juancastillo2202@gmail.com>
2024-11-19 10:37:52 -0600

    se configuro securityconfig para java 17

--- src/main/java/tienda/config/SecurityConfig.java
+++ src/main/java/tienda/config/SecurityConfig.java
@@ -1,17 +1,20 @@
 package tienda.config;
 
-import tienda.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import tienda.services.UserService;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final UserService userService;
@@ -31,27 +34,28 @@ public class SecurityConfig {
     }
 
     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/login", "/register").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .permitAll()
+            );
+
+        return http.build();
+    }
+
+    @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
         return authenticationManagerBuilder.build();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests()
-                .requestMatchers("/login", "/register").permitAll()  // Permite acceso sin autenticación a estas rutas
-                .anyRequest().authenticated()  // Resto de las rutas requieren autenticación
-            .and()
-            .formLogin()
-                .loginPage("/login")  // Página personalizada de login
-                .permitAll()
-            .and()
-            .logout()
-                .permitAll();
-
-        return http.build();
     }
 }
